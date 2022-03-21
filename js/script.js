b5d42bbbc5116da363dd73ab5a559a117cb49f60
MariiaPolishchuk <mariia.poli91@gmail.com>
2022-03-21 22:31:21 +0200

    tasks 6 and 7 were added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ console.log(2 - 1);
 console.log(3 * 4);
 console.log(12 / 2);
 console.log('6');
+console.log(1, 2, 3, 4, 5);
+console.log('6(just training)')
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+console.log('7');
+console.log(5, 4, 3, 2, 1);
+console.log('8');
